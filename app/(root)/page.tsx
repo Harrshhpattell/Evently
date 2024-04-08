66901496eaf76c2a23e802fb29e961d729c11fa2
Patel Harsh <harshpatel8320@gmail.com>
2024-04-08 22:57:46 +0530

    complete orders page. search func and category func

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
-import Collection from "@/components/shared/Collection";
-import { Button } from "@/components/ui/button";
-import { getAllEvents } from "@/lib/action/event.actions";
-import { SearchParamProps } from "@/types";
-import Image from "next/image";
-import Link from "next/link";
+
+import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
+import { Button } from '@/components/ui/button'
+import { getAllEvents } from '@/lib/action/event.actions';
+import { SearchParamProps } from '@/types';
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function Home({ searchParams }: SearchParamProps) {
   const page = Number(searchParams?.page) || 1;
@@ -53,7 +56,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
           Trust by <br /> Thousands of Events
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search CategoryFilter
+          <Search />
+          <CategoryFilter />
         </div>
         <Collection
           data={events?.data}

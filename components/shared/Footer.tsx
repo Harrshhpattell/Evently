97699e1694d3979863aa2dfca7a378dca4b7778e
Patel Harsh <harshpatel8320@gmail.com>
2024-05-05 13:48:39 +0530

    Update Footer.tsx

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
+    <>
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href="/">
@@ -18,6 +19,12 @@ const Footer = () => {
         <p>2024 Evently. All Rights reserved.</p>
       </div>
     </footer>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <p style={{color: "green"}}>This project is created for internship(GTU) purposes only.</p>
+      </div>
+    </footer>
+    </>
   );
 };
 
